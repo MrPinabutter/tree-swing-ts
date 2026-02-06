250aeaecdeeb9058a022e21672ce3b7736ed2033
vitu <vitorafael.r.c@gmail.com>
2026-02-06 16:33:14 -0300

    refactor: simplify menuLoaders by removing unused menu states

--- navigate/index.ts
+++ navigate/index.ts
@@ -3,16 +3,7 @@ import { hideCursor } from "../core/terminal/cursor";
 import { setMenuState } from "../state";
 
 const menuLoaders = {
-  [MENU_STATE.MAIN]: () =>
-    import("../menus/start").then((mod) => mod.showMenuStart()),
-  [MENU_STATE.SELECT_FORM]: () =>
-    import("../menus/answerForm").then((mod) => mod.showMenuAnswerForm()),
-  [MENU_STATE.CREATE_FORM]: () =>
-    import("../menus/createForm").then((mod) => mod.showMenuCreateForm()),
-  [MENU_STATE.LOOK_ANSWERS]: () =>
-    import("../menus/lookAnswers").then((mod) => mod.showMenuLookAnswers()),
-  [MENU_STATE.REMOVE_FORM]: () =>
-    import("../menus/removeForm").then((mod) => mod.showRemoveForm()),
+  [MENU_STATE.MAIN]: () => {},
 } as const;
 
 export const navigateToMenu = (menu: MENU_STATE) => {
